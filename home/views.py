712eab9cdc6a5741d4487d1295c8942d50eeeee7
saurav-vinodh <sauravvinodh2003@gmail.com>
2023-03-23 20:28:37 +0530

    RENDERING THE FORMS

--- home/views.py
+++ home/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def home(request):
+    if request.method == "POST":
+        
+        contact = request.POST[]
+
     return render(request, 'page.html')
