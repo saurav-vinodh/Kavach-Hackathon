8d1114909add655d66ec622430eae365cede98a4
saurav-vinodh <sauravvinodh2003@gmail.com>
2023-03-23 20:05:14 +0530

    Adding models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Entry(models.Model):
+    
+
+    contact = models.CharField(max_length=200)
+    source = models.CharField(max_length=200)
+    weight= models.IntegerField(default=1)
+    date = models.DateTimeField(auto_now_add=True)
+    userid = models.AutoField(primary_key=True)
+    risk_score = models.IntegerField()
+    spam = models.IntegerField()
+
+    
+    class Meta:
+        unique_together=('userid', 'contact',)
+
