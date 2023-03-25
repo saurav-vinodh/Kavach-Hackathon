b2e7fa5748d5cdaec9f69c96677f9a57dd48db0d
saurav-vinodh <sauravvinodh2003@gmail.com>
2023-03-25 13:34:28 +0530

    Finished the overwriting and database inputs

--- home/views.py
+++ home/views.py
@@ -9,11 +9,19 @@ def home(request):
         name_v = request.POST.get('name')
         spam_v = request.POST.get('spam-yes')
         risk_score_v = request.POST.get('myRange')
+
+        if not(name_v):
+            name_v=request.POST.get('company')
+        
+
         print(contact_v, name_v, spam_v, risk_score_v)
         if (spam_v=='yes'):
             spam_value=1
         else:
             spam_value=0
+            risk_score_v=0
+        
+        Entry.objects.filter(userid = 'Saurav', contact=contact_v).delete()
         
         ins = Entry(contact=contact_v, source=name_v, risk_score=risk_score_v,spam=spam_value)
         ins.save()
